8fd0a0dcae3643841798944cb3304c38d6968d07
Satyrn <pastawho@gmail.com>
2022-01-27 01:18:38 +0000

    add some stuff

--- vapor.js
+++ vapor.js
@@ -1,38 +1,54 @@
 var wss = null;
+var connected = false;
+var new_message = false;
+var message = "";
+var username = ""
 
 class vapor {
   constructor(runtime, extensionId) {
     this.runtime = runtime;
-  }
+  };
 	static get STATE_KEY() {
-		return 'Scratch.websockets';
-	}
+		return "Scratch.websockets";
+	};
   getInfo() {
     return {
       id: "vapor",
       name: "Vapor",
       blocks: [
       {
-        opcode: "connectToServer",
+        opcode: "connect_to_server",
         blockType: Scratch.BlockType.COMMAND,
         text: "connect to server [url]",
         arguments: {
           url: {
             type: Scratch.ArgumentType.STRING,
             defaultValue: "ws://127.0.0.1:3000",
-            },
           },
-        }
-      ],
+        },
+      }, {
+        opcode: "get_current_message",
+        blockType: Scratch.BlockType.REPORTER,
+        text: "current message",
+      }],
     }
-  }
+  };
     
-  connectToServer({url}) {
-    wss = new WebSocket(url)
-    wss.onopen = function(e) {
-      wss.send("new connection")
-    }
-  }
-}
+  connect_to_server({url}) {
+    wss = new WebSocket(url);
+    wss.onopen = function() {
+      connected = true;
+      wss.send("new connection");
+    };
+    wss.onmessage = function(event) {
+      message = String(event.data);
+      new_message = true;
+    };
+  };
 
-Scratch.extensions.register(new vapor())
+  get_current_message() {
+    return message;
+  };
+};
+
+Scratch.extensions.register(new vapor());
