07f451c0e08263cd33cbd57f8c9770de8c416eb2
Satyrn <pastawho@gmail.com>
2022-01-28 15:08:40 +0000

    welcome to JS, the language where you "need" semicolons but really dont

--- vapor.js
+++ vapor.js
@@ -1,20 +1,20 @@
 // Variables
-var wss = null;
-var connected = false;
-var message = "";
-var username = "";
-var password = "";
-var new_message = false;
-var old_message_state = false;
+var wss = null
+var connected = false
+var message = ""
+var username = ""
+var password = ""
+var new_message = false
+var old_message_state = false
 
 // Class
 class vapor {
 	constructor(runtime) {
-		this.runtime = runtime;
-	};
+		this.runtime = runtime
+	}
 	static get STATE_KEY() {
-		return "Scratch.websockets";
-	};
+		return "Scratch.websockets"
+	}
   getInfo() {
     return {
       id: "vapor",
@@ -67,86 +67,86 @@ class vapor {
         blockType: Scratch.BlockType.REPORTER,
         text: "current message",
       }],
-    };
-  };
+    }
+  }
 
   // Connected - Boolean
   connected_to_server() {
     if (wss != null && get_boolean(connected)) {
-      return true;
+      return true
     } else {
-      return false;
-    };
-  };
+      return false
+    }
+  }
 
   // Connect To Server - Command
   connect_to_server({url}) {
-    wss = new WebSocket(url);
-    wss.onopen = function(e) {
-      wss.send("new_conn");
-    };
+    wss = new WebSocket(url)
+    wss.onopen = function() {
+      wss.send("new_conn")
+    }
     wss.onmessage = function(event) {
-      message = String(event.data);
+      message = String(event.data)
       if (message == "conn_deny") {
-        disconnect_from_server();
+        disconnect_from_server()
       } else if (message == "conn_accept") {
-        //message = "";
-        connected = true;
+        connected = true
+        message = ""
       } else {
-        new_message = true;
-      };
-    };
-  };
+        new_message = true
+      }
+    }
+  }
 
   // When New Message Recieved - Hat
   when_new_message() {
-    old_message_state = new_message;
-    new_message = false;
-    return get_boolean(old_message_state);
-  };
+    old_message_state = new_message
+    new_message = false
+    return get_boolean(old_message_state)
+  }
 
   // Get Data From URL - Reporter
   get_url_data({url}) {
-    return fetch(url).then(response => response.text());
-  };
+    return fetch(url).then(response => response.text())
+  }
 
   // Current Message - Reporter
   get_current_message() {
-    return message;
-  };
+    return message
+  }
 
   // Send Message - Command
   send_message({msg}) {
     if (wss != null && get_boolean(connected)) {
-      wss.send(String(msg));
+      wss.send(String(msg))
     };
-  };
+  }
 
   // Boolean Tester
   get_boolean(value) {
     switch(value) {
       case true:
       case "true":
-        return true;
+        return true
       default:
-        return false;
-    };
-  };
+        return false
+    }
+  }
 
   // Disconnect From Server - Command
   disconnect_from_server() {
     if (get_boolean(connected)) {
       wss.send("close_conn")
-      wss.close(1000);
-      connected = false;
-      message = "";
-      username = "";
-      password = "";
-      wss = null;
-      new_message = false;
-    };
-  };
-};
+      wss.close(1000)
+      connected = false
+      message = ""
+      username = ""
+      password = ""
+      wss = null
+      new_message = false
+    }
+  }
+}
 
 // Register Extension
-Scratch.extensions.register(new vapor());
+Scratch.extensions.register(new vapor())
